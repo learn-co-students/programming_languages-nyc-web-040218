4ba1157714ef311e2d03f2a80b53ed0ad432df83
Lane Miller <chris.lane.miller@gmail.com>
2018-03-22 19:26:41 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,27 @@
+# {Style:
+#   {Language:
+#     {type: value}
+
+#Reformatted to:
+
+# {Language:
+#   {type: value, style: [value(s)] } (style values are arrays)
+
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  style_array = [] 
+  
+  languages.each do |style, lang_data|
+    lang_data.each do |lang, type_data|
+      if new_hash[lang] == nil
+        new_hash[lang] = type_data
+        new_hash[lang][:style] = [style]
+      else
+        new_hash[lang][:style] << style
+      end
+    end
+  end
+  new_hash
 end

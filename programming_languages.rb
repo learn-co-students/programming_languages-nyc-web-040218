f3195d60f4814c3bf8b0e017a2ad64f8d3d440aa
Daniel Brazel <dbrazel4@gmail.com>
2018-03-23 14:53:05 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  answer = Hash.new
+  languages.each do |style, langs|
+    langs.each do |lang, type|
+      type.each do |key1, val1|
+        if answer.include? lang
+          answer[lang][:style].push(style)
+        else
+          answer[lang] = {key1 => val1, :style => [] << style}
+        end
+      end
+    end
+  end
+  answer
 end

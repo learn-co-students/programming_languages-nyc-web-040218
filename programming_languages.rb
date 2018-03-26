fbbc2fc6b7a39118d338adcc85a8538f8e6bf3b8
Ryan Hu <ryanhuuu@gmail.com>
2018-03-26 23:45:00 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
+  result = {}
+  
+  
+  languages.each do |style, style_values|
+    style_values.each do |language, language_values|
+      if !result.has_key?(language)
+        temp = {}
+        temp[:type] = language_values[:type]
+        temp[:style] = [style]
+        result[language.to_sym] = temp
+      else
+        result[language.to_sym][:style] << style
+      end
+    end
+  end
+  return result
 end
